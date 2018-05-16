711c7aa44a89ded20a3fbfe6b615843d6dd02fd1
Parag <Parag@LAPTOP-PARAG>
2018-05-16 21:35:34 +0530

    Spring Boot Application

---
+++ SpringBoot/src/main/java/com/parag/springboot/MainApplication.java
@@ -0,0 +1,16 @@
+package com.parag.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(MainApplication.class, args);
+		
+
+	}
+
+}
